3de2824531e2793250b45fcbb68652e3cdcc5938
Salma Hanafi <shanafi2017@gmail.com>
2021-02-17 14:24:41 +0200

    Extension Methods

---
+++ Extensions/IntExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Both class and extension method need to be static
+namespace Extensions
+{
+    public static class IntExtensions
+    {
+        public static Boolean IsGreaterThan (this int i, int value)
+        {
+            return i > value;
+        }
+    }
+}
---
+++ Extensions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Extensions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int num = 101;
+        Console.WriteLine(num.IsGreaterThan(102));
+        }
+    }
+
