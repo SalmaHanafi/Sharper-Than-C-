5f94a5eb0cc706b43f1e7845ae5a71cdbaf3f7ff
Salma Hanafi <shanafi2017@gmail.com>
2021-03-08 16:56:39 +0200

    Adding a project to interact with data using EF

---
+++ EF/NinjaDomain/ConsoleApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NinjaDomain.Classes;
+using NinjaDomain.DataModel;
+using System.Data.Entity;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new NullDatabaseInitializer<NinjaContext>());
+            InsertNinja();
+            Console.ReadKey();
+        }
+        private static void InsertNinja()
+        {
+            var ninja = new Ninja
+            {
+                Name = "Sampson",
+                ServedInOniwaban = false,
+                DateOfBirth = new DateTime(2009, 10, 20),
+                ClanId = 1
+            };
+            using (var context = new NinjaContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                context.Ninjas.Add(ninja);
+                context.SaveChanges();
+            }
+        }
+    }
+}
