d7b50f5adad6a26fe9ba288ba434c68f9e9e9a40
Salma Hanafi <shanafi2017@gmail.com>
2021-03-11 15:51:06 +0200

    Lambda expressions, Func and Action

--- linq-fundamentals-csharps-6/2-linq-fundamentals-csharp-6-m2-exercise-files/before/Features/Program.cs
+++ linq-fundamentals-csharps-6/2-linq-fundamentals-csharp-6-m2-exercise-files/before/Features/Program.cs
@@ -9,7 +9,9 @@ namespace Features
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
+
+           
             Employee[] developers = new Employee[]
             {
                 new Employee { Id = 1, Name= "Scott" },
@@ -20,7 +22,65 @@ namespace Features
             {
                 new Employee { Id = 3, Name = "Alex" }
             };
-            
+            foreach (var employee in developers.Where(n => n.Name.Length == 5 ).OrderBy(n => n.Name))
+                
+            {
+                Console.WriteLine(employee.Name);
+            }
+            Console.ReadLine();
+
+            #region enumerators
+
+            // //IEnumberable is the perfect interface for hiding
+            // //the source of data
+
+            // IEnumerable<Employee> developers = new Employee[]
+            //{
+            //      new Employee { Id = 1, Name= "Scott" },
+            //      new Employee { Id = 2, Name= "Chris" }
+            //};
+
+            // IEnumerable<Employee> sales = new List<Employee>()
+            //  {
+            //      new Employee { Id = 3, Name = "Alex" }
+            //  };
+
+            //IEnumerator<Employee> enumerator = developers.GetEnumerator();
+
+            ////extension method
+            //Console.WriteLine(developers.Count());
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Name);
+            //}
+
+            //foreach (var employee in developers.Where(n => n.Name.StartsWith("S")))
+            //{
+            //    Console.WriteLine(employee.Name);
+            //}
+            //Console.ReadLine();
+            #endregion
+
+            #region Func
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> sum = (x, y) => x + y;
+            Func<int, int, int> sumThenSquare = (a, b) =>
+            {
+                int summation = a + b;
+                return square(summation);
+            };
+
+            Console.WriteLine(square(2));
+            Console.WriteLine(sumThenSquare(2,3));
+            Console.ReadLine();
+            #endregion
+
+            #region Action
+            Action<int> write = x => Console.WriteLine($"From Action {x}");
+
+            write(5);
+            Console.ReadLine();
+            #endregion
         }
     }
 }
