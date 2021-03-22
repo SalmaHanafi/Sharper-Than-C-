6eeef1b80dd4e8bad2d81a540e22c5880871e515
Salma Hanafi <shanafi2017@gmail.com>
2021-03-22 11:41:46 +0200

    filters by using first

--- linq-fundamentals-csharps-6/4-linq-fundamentals-csharp-6-m4-exercise-files/before/Cars/Program.cs
+++ linq-fundamentals-csharps-6/4-linq-fundamentals-csharp-6-m4-exercise-files/before/Cars/Program.cs
@@ -12,11 +12,22 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
-            //var query = cars.OrderByDescending(c => c.Combined).ThenBy(c => c.Name);
+            //var top = cars.Where(a => a.Manufacturer == "BMW" && a.Year == 2016)
+            //            .OrderByDescending(c => c.Combined)
+            //            .ThenBy(c => c.Name)
+            //            .First();
+
             var query = from car in cars
+                        where car.Manufacturer == "BMW" && car.Year == 2016
                         orderby car.Combined descending, car.Name
                         select car;
 
+            var top = cars.OrderByDescending(c => c.Combined)
+                          .ThenBy(c => c.Name)
+                          .First(a => a.Manufacturer == "BMW" && a.Year == 2016);
+
+            Console.WriteLine($"From method syntax: {top.Name} : {top.Combined}");
+            Console.WriteLine($"From query syntax: {query.First().Name} : {query.First().Combined}");
             foreach (var car in query.Take(10))
             {
                 Console.WriteLine($"{car.Name} : {car.Combined}");
