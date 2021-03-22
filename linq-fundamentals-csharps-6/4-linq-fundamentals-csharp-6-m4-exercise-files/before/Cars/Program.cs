b6a09f040c98ce85dad587b239ac8d7c53ba45a2
Salma Hanafi <shanafi2017@gmail.com>
2021-03-22 12:52:10 +0200

    uses SelectMany for projection and flattening

--- linq-fundamentals-csharps-6/4-linq-fundamentals-csharp-6-m4-exercise-files/before/Cars/Program.cs
+++ linq-fundamentals-csharps-6/4-linq-fundamentals-csharp-6-m4-exercise-files/before/Cars/Program.cs
@@ -12,33 +12,62 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
+            #region filtering and ordering 
             //var top = cars.Where(a => a.Manufacturer == "BMW" && a.Year == 2016)
             //            .OrderByDescending(c => c.Combined)
             //            .ThenBy(c => c.Name)
             //            .First();
+            //var top = cars.OrderByDescending(c => c.Combined)
+            //              .ThenBy(c => c.Name)
+            //              .First(a => a.Manufacturer == "BMW" && a.Year == 2016);
+
+            //var query = from car in cars
+            //            where car.Manufacturer == "BMW" && car.Year == 2016
+            //            orderby car.Combined descending, car.Name
+            //            select car;
+            //Console.WriteLine($"From query syntax: {query.First().Name} : {query.First().Combined}");
+            //foreach (var car in query.Take(10))
+            //{
+            //    Console.WriteLine($"{car.Name} : {car.Combined}");
+            //}
+
+            //var av = cars.Any(c => c.Year == 2016);
+            //Console.WriteLine(av);
+            //Console.WriteLine($"From method syntax: {top.Name} : {top.Combined}");
+            #endregion
 
             var query = from car in cars
-                        where car.Manufacturer == "BMW" && car.Year == 2016
+                        where car.Manufacturer == "bmw" && car.Year == 2016
                         orderby car.Combined descending, car.Name
-                        select car;
+                        select new
+                        {
+                            car.Manufacturer,
+                            car.Name,
+                            car.Combined
 
-            var top = cars.OrderByDescending(c => c.Combined)
-                          .ThenBy(c => c.Name)
-                          .First(a => a.Manufacturer == "BMW" && a.Year == 2016);
+                        };
 
-            Console.WriteLine($"From method syntax: {top.Name} : {top.Combined}");
-            Console.WriteLine($"From query syntax: {query.First().Name} : {query.First().Combined}");
-            foreach (var car in query.Take(10))
+            //looking at the string as a sequence of characters
+            var result = cars.SelectMany(c => c.Name)
+                              .OrderBy(c => c)
+                              .GroupBy(c => c);
+                             
+            foreach (var character in result)
             {
-                Console.WriteLine($"{car.Name} : {car.Combined}");
+                Console.WriteLine($"{character.Key} : {character.Count()}");
             }
-        }
-
+        
+        }   
         private static List<Car> ProcessFile(string path)
         {
-            var query = from line in File.ReadAllLines(path).Skip(1)
-                        where line.Length > 1
-                        select Car.ParseFromCsv(line);
+            //var query = from line in File.ReadAllLines(path).Skip(1)
+            //            where line.Length > 1
+            //            select Car.ParseFromCsv(line);
+
+            var query = File.ReadAllLines(path)
+                            .Skip(1)
+                            .Where(l => l.Length > 1)
+                            .ToCar();
             return query.ToList();
                
                  
