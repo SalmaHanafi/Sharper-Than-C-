4f4da86db8266a84883851d4fb06990c1e74025e
Salma Hanafi <shanafi2017@gmail.com>
2021-03-22 09:57:47 +0200

    uses secondary sort

--- linq-fundamentals-csharps-6/4-linq-fundamentals-csharp-6-m4-exercise-files/before/Cars/Program.cs
+++ linq-fundamentals-csharps-6/4-linq-fundamentals-csharp-6-m4-exercise-files/before/Cars/Program.cs
@@ -12,16 +12,30 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
+            //var query = cars.OrderByDescending(c => c.Combined).ThenBy(c => c.Name);
+            var query = from car in cars
+                        orderby car.Combined descending, car.Name
+                        select car;
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Name} : {car.Combined}");
+            }
         }
 
         private static List<Car> ProcessFile(string path)
         {
-          return 
-                File.ReadAllLines(path)
-                .Skip(1)
-                .Where(line => line.Length > 1)
-                .Select(Car.ParseFromCsv)
-                .ToList();
+            var query = from line in File.ReadAllLines(path).Skip(1)
+                        where line.Length > 1
+                        select Car.ParseFromCsv(line);
+            return query.ToList();
+               
+                 
+                //File.ReadAllLines(path)
+                //.Skip(1)
+                //.Where(line => line.Length > 1)
+                //.Select(Car.ParseFromCsv)
+                //.ToList();
         }
 
     }
