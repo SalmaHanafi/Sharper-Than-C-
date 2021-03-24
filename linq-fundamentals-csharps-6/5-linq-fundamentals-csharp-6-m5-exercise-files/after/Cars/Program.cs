e7ce38f7ebda496fbe4e1274c85c800c7f2cee86
Salma Hanafi <shanafi2017@gmail.com>
2021-03-24 12:13:56 +0200

    queries xml

--- linq-fundamentals-csharps-6/5-linq-fundamentals-csharp-6-m5-exercise-files/after/Cars/Program.cs
+++ linq-fundamentals-csharps-6/5-linq-fundamentals-csharp-6-m5-exercise-files/after/Cars/Program.cs
@@ -13,7 +13,23 @@ namespace Cars
         static void Main(string[] args)
         {
             CreateXml();
+            QueryXml();
+        }
 
+        private static void QueryXml()
+        {
+            var document = XDocument.Load("fuel.xml");
+
+            var query = 
+                from element in document.Element("Cars").Elements("Car")
+                //from element in document.Descendants("Car")
+                where element.Attribute("Manufacturer")?.Value == "BMW"
+                select element.Attribute("Name").Value;
+
+            foreach (var name in query)
+            {
+                Console.WriteLine(name);
+            }
         }
 
         private static void CreateXml()
