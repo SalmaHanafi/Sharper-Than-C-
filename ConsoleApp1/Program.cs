6546414f68420d993b0d793d944742a7ceb985f4
Salma Hanafi <shanafi2017@gmail.com>
2021-04-01 11:52:45 +0200

    sqlkata

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using SqlKata;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create an instance of SQLServer
+            var compiler = new SqlServerCompiler();
+
+            var query = new Query("Users").Where("Id", 1).Where("Status", "Active");
+
+            SqlResult result = compiler.Compile(query);
+
+            string sql = result.Sql;
+            List<object> bindings = result.Bindings; // [ 1, "Active" ]
+            Console.WriteLine(bindings[1]);
+        }
+    }
+}
